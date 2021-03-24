fb82dcfc7ca1ab80d312dba1bae8e0be24a4f4cc
mjmorkin <michaeljmorkin@gmail.com>
2021-03-24 13:03:56 -0500

    Initial Commit

---
+++ github-xcode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  github-xcode
+//
+//  Created by Michael Morkin on 3/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ github-xcode/github_xcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_xcodeApp.swift
+//  github-xcode
+//
+//  Created by Michael Morkin on 3/24/21.
+//
+
+import SwiftUI
+
+@main
+struct github_xcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
